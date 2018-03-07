9a043920caec2d45312c2dd3ca81e8934ba2b759
Anthony Dresser <anthonydresserdev@gmail.com>
2018-03-06 18:15:36 -0800

    release

---
+++ out/main.js
@@ -0,0 +1,8 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./serverProvider"));
+__export(require("./serviceDownloadProvider"));
+//# sourceMappingURL=main.js.map
